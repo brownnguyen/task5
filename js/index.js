9dd9dfcb13f2a30f33e39d85ab160f66d8cf0f4c
unknown <brownnguyen268@gmail.com>
2020-07-22 08:23:40 +0700

    add

---
+++ js/index.js
@@ -0,0 +1,81 @@
+$('.top-slider-main').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                dots: false
+            }
+        }
+    ]
+
+});
+$('.slick-interview').slick({
+});
+
+let header = document.querySelector('.header');
+window.onscroll = function () {
+    if (window.pageYOffset > 30) {
+        header.classList.add('stick')
+    }
+    else {
+        header.classList.remove('stick')
+    }
+    if (window.pageYOffset > window.innerHeight / 2) {
+        document.querySelector('#backTop').style.opacity = 1;
+        document.querySelector('#backTop').style.visibility = "visible"
+    }
+    else {
+        document.querySelector('#backTop').style.opacity = 0;
+        document.querySelector('#backTop').style.visibility = "hidden"
+    }
+}
+let navBtn = document.querySelector('.nav-btn');
+let drawer = document.querySelector('.drawer');
+navBtn.onclick = function () {
+    navBtn.classList.toggle('expand');
+    if (navBtn.classList.contains('expand')) {
+        drawer.classList.add("expand")
+    }
+    else {
+        drawer.classList.remove("expand")
+    }
+}
+let slickLeft = document.querySelector('.interview .slick-prev');
+let btnLeft = document.createElement('span');
+btnLeft.classList.add('arrow-new-left');
+slickLeft.appendChild(btnLeft);
+
+let slickRight = document.querySelector('.interview .slick-next');
+let btnRight = document.querySelector('span');
+btnRight.classList.add("arrow-new-right")
+slickRight.appendChild(btnRight);
+let dataUl = document.querySelectorAll('.ul-main');
+let contentHeadUl = document.querySelectorAll('.content.head-ul');
+for (let i = 0; i < contentHeadUl.length; i++) {
+    contentHeadUl[i].onclick = function () {
+        for (let k = 0; k < dataUl.length; k++) {
+            dataUl[k].style.height = 0;
+        }
+        dataUl[i].classList.toggle('expand');
+        if (dataUl[i].classList.contains('expand')) {
+            let height = 0;
+            for (let j = 0; j < dataUl[i].children.length; j++) {
+                height += parseFloat(getComputedStyle(dataUl[i].children[j], null).height.replace("px", ""));
+            }
+            dataUl[i].style.height = height + "px";
+        }
+        else {
+            dataUl[i].style.height = 0;
+        }
+    }
+}
+
+$('#backTop').click(function (event) {
+    $('html,body').animate({
+        scrollTop: 0
+    }, 1000, 'swing');
+    return false;
+});
